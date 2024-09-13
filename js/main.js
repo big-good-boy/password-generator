6250c5b91105c5aae213993a6abdc30a03c1542a
big-good-boy <good_boy@live.ru>
2024-09-13 17:53:03 +0500

    вынес html код в js файл и наметил структуру js файлов

--- js/main.js
+++ js/main.js
@@ -1,5 +1,84 @@
 'use strict';
 
+const body = document.createElement('div');
+body.innerHTML = `
+<div class="pass-generator">
+<div class="pass-generator__wrapper">
+	<div class="pass-generator__length">
+		<div class="pass-generator__length-top">
+			<span class="pass-generator__length-text">Длина пароля:</span>
+			<span class="pass-generator__length-wrap">
+				<span
+					class="pass-generator__length-num pass-generator__length-num--less"
+					>-</span
+				>
+				<input
+					class="pass-generator__length-input"
+					type="number"
+					value="8"
+					min="6"
+					max="24"
+				/>
+				<span
+					class="pass-generator__length-num pass-generator__length-num--more"
+					>+</span
+				>
+			</span>
+			<span class="pass-generator__length-text">символов</span>
+		</div>
+		<input
+			class="pass-generator__length-range"
+			type="range"
+			min="6"
+			max="24"
+			value="8"
+		/>
+	</div>
+
+	<div class="pass-generator__result">
+		<span class="pass-generator__result-pass">00000000</span>
+		<span class="pass-generator__result-link"
+			>Скопировать в буфер обмена</span
+		>
+	</div>
+
+	<ul class="pass-generator__option">
+		<li
+			class="pass-generator__option-item pass-generator__option-item--active pass-generator__option-item--upper"
+		>
+			Верхний регистр
+		</li>
+		<li
+			class="pass-generator__option-item pass-generator__option-item--active pass-generator__option-item--lower"
+		>
+			Нижний регистр
+		</li>
+		<li
+			class="pass-generator__option-item pass-generator__option-item--active pass-generator__option-item--num"
+		>
+			Цифры
+		</li>
+		<li
+			class="pass-generator__option-item pass-generator__option-item--char"
+		>
+			Символы
+			<span
+				class="pass-generator__option-info"
+				data-title="! # $ % & ( ) * + . / : ; = > ? @ [ \ ] ^ \` { | } ~ ' - < _ >"
+			></span>
+		</li>
+		<li
+			class="pass-generator__option-item pass-generator__option-item--white"
+		>
+			Пробел
+		</li>
+	</ul>
+</div>
+</div>
+`;
+
+document.body.appendChild(body);
+
 // Словари
 var upp = [
 	'A',
